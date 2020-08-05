d58eae3f0e72f482dedf02f0b109cfeaae2888f9
guru.bagawan <guru.bagawan@gmail.com>
2020-08-05 14:02:19 -0400

    main layout done

--- fintros-test/src/App.js
+++ fintros-test/src/App.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple } from '@material-ui/core/colors';
+import { AppBar } from '@material-ui/core';
 import BlogItem from './components/blog-card';
+import { Row, Container } from 'react-bootstrap';
+import Spinner from './components/spinner';
+
+import { useBottomScrollListener } from 'react-bottom-scroll-listener';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#212121',
+      background: '#F0F0F0',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
 
 function App() {
+  const [postIDs, setIDs] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    fetch('https://hacker-news.firebaseio.com/v0/jobstories.json?print=pretty')
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        setIDs(result);
+        setLoaded(true);
+      });
+    // return () => {
+    //   cleanup;
+    // };
+  }, []);
+
+  useBottomScrollListener(() => setPage(page + 1));
+
+  const displayPosts = postIDs.map((id, i) => {
+    if (i < 30 * page) return <BlogItem key={i} itemID={id} />;
+  });
+  if (!loaded) return <Spinner />;
   return (
-    <div className="App">
-      <BlogItem itemID={8863} />
-    </div>
+    <Container>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <AppBar> Blog posts</AppBar>
+          <Row>{displayPosts}</Row>
+        </div>
+      </ThemeProvider>
+    </Container>
   );
 }
 
--- fintros-test/src/components/blog-card.js
+++ fintros-test/src/components/blog-card.js
@@ -1,22 +1,75 @@
 import React, { useState, useEffect } from 'react';
+import { Col, Row } from 'react-bootstrap';
+// import grabity from 'grabity';
+// import { Container, Row, Col } from 'reactstrap';
+import Spinner from './spinner';
+import Box from '@material-ui/core/Box';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { purple } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+
+import { getLinkPreview } from 'link-preview-js';
+
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: {
+//       // Purple and green play nicely together.
+//       main: purple[500],
+//     },
+//     secondary: {
+//       // This is green.A700 as hex.
+//       main: '#11cb5f',
+//     },
+//   },
+// });
+
+// let grabity = require('grabity');
+
+// (async () => {
+//   let it = await grabity.grabIt(
+//     'https://hacker-news.firebaseio.com/v0/item/24061224.json?print=pretty'
+//   );
+
+//   console.log(it);
+// })();
 
 const BlogItem = ({ itemID }) => {
   const [loaded, setLoaded] = useState(false);
   const [post, setPost] = useState({});
 
   useEffect(() => {
+    // (async () => {
+    //   let it = await grabity.grab(
+    //     `https://hacker-news.firebaseio.com/v0/item/${itemID}.json?print=pretty`
+    //   );
+
+    //   console.log(it);
+    // })();
     fetch(
       `https://hacker-news.firebaseio.com/v0/item/${itemID}.json?print=pretty`
     )
       .then((res) => res.json())
       .then((result) => {
-        console.log(result);
         setLoaded(true);
         setPost(result);
       });
-  }, {});
-  console.log(itemID, post);
-  return <div>{post.title}</div>;
+  }, []);
+  if (!loaded) return <Spinner />;
+  return (
+    <Col style={{ marginBottom: 10 }} xs={12} sm={6} md={4}>
+      {/* <ThemeProvider theme={theme}> */}
+      <Box
+        style={{ padding: 10, height: `100%` }}
+        bgcolor="primary.background"
+        color="primary.main"
+      >
+        <div>{post.title}</div>
+        <div>{post.text}</div>
+      </Box>
+      {/* </ThemeProvider> */}
+    </Col>
+  );
 };
 
 export default BlogItem;
